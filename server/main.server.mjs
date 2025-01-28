1a798bec45fe04c1a7952a066e79d0a4d432df4c
Nikoloz Dvali <msndvali@gmail.com>
2025-01-28 12:56:45 +0400

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-I6BSOJUF.mjs";import"./chunk-HQKYKJN4.mjs";import"./chunk-GTSSCL6B.mjs";import"./chunk-V2EO6222.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
