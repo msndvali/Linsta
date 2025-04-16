161e42face7c8b7c1c12c51e2b1002ede19636e6
Nikoloz Dvali <ndvali@sitelint.com>
2025-04-16 10:58:08 +0200

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4I7CXMQP.mjs";import"./chunk-HQKYKJN4.mjs";import"./chunk-GTSSCL6B.mjs";import"./chunk-V2EO6222.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
